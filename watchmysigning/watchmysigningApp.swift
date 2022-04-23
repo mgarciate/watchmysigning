5d095293e0551c697eec0d6eece97bd521ed5698
mgarciate <mgarciate@gmail.com>
2022-04-23 13:29:35 +0200

    Initial commit

---
+++ watchmysigning/watchmysigningApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchmysigningApp.swift
+//  watchmysigning
+//
+//  Created by mgarciate on 23/4/22.
+//
+
+import SwiftUI
+
+@main
+struct watchmysigningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
